9f6aef69ca24f6c1db8b66b0b1b5aa8ffd3ddd29
tzarbasil <atreides03@mail.ru>
2025-02-10 16:49:33 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
